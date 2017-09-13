990c57d63df07f2776e4964470a80e9f319cef77
LiamTyler <tyler.liam7@gmail.com>
2017-09-12 23:19:09 -0500

    test(proj): testing php includes

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<?php
+echo '<div class="navbar">
+  <nav>
+    <ul>
+      <div class="floating">
+        <li id="LT-header">Liam Tyler<p>Software Developer</p></li>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="portfolio.html">Portfolio</a></li>
+      </div>
+    </ul>
+  </nav>
+</div>';
+?>
