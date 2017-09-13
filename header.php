e4939f67f325a417fd03f9436df9d1316be99ac1
remote <tyler147@umn.edu>
2017-09-13 17:20:01 -0500

    fix(absoloute paths): Fixed absolute paths

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 echo "<meta charset='utf-8'>
   <title>Liam Tyler</title>
-  <link rel='stylesheet' type='text/css' href='http://www.liamtyler.com/styles/styles.css'>
-  <script src='scripts/jquery-3.2.1.js'></script>";
+  <link rel='stylesheet' type='text/css' href='/styles/styles.css'>
+  <script src='/scripts/jquery-3.2.1.js'></script>";
 ?>
--- navbar.php
+++ navbar.php
@@ -4,9 +4,10 @@ echo '<div class="navbar">
     <ul>
       <div class="floating">
         <li id="LT-header">Liam Tyler<p>Software Developer</p></li>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="portfolio.html">Portfolio</a></li>
+        <li><a href="/index.html">Home</a></li>
+        <li><a href="/about.html">About</a></li>
+        <li><a href="/portfolio.html">Portfolio</a></li>
+        <li><a href="/work_research.html">Work & Research</a></li>
       </div>
     </ul>
   </nav>
