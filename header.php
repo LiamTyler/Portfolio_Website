9df77d4929ac18eb22709b6241456b6cda92d82b
LiamTyler <tyler.liam7@gmail.com>
2017-11-27 21:17:21 -0600

    test(analytics): trying to add analytics

--- header.php
+++ header.php
@@ -2,5 +2,14 @@
 echo "<meta charset='utf-8'>
   <title>Liam Tyler</title>
   <link rel='stylesheet' type='text/css' href='/styles/styles.css'>
-  <script src='/scripts/jquery-3.2.1.js'></script>";
+  <script src='/scripts/jquery-3.2.1.js'></script>
+  <!-- Globbal site tag (gtag.js) - Google Analytics -->
+  <script async src='https://www.googletagmanager.com/gtag/js?id=UA-110304402-1'></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-110304402-1');
+  </script>";
 ?>
